0e5ff7a1734059f39527c3a30e2798064e64e7fd
John-Laby <john@lab-y.kr>
2018-05-03 18:15:53 +0900

    빈칸 한줄 추가

--- src/main/java/com/eshelsoft/mongo/CustomerRepository.java
+++ src/main/java/com/eshelsoft/mongo/CustomerRepository.java
@@ -8,4 +8,5 @@ public interface CustomerRepository extends MongoRepository<Customer, String> {
 
     Customer findByFirstName(String firstName);
     List<Customer> findByLastName(String lastName);
+
 }
